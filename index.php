721cbfe3c99d5d030f6bdf314e56d70c21ee1e04
OliverMute <olivieralexis@yahoo.com>
2018-11-16 20:34:58 +0100

    Première version de mon projet

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+
+
+	<div class="container">
+	<h1 class="text-center">PRODUCTS</h1>
+		<div class="row">
+			<div class="col-md-3">
+				<a href="">Produit 1</a>
+				<img class="img-fluid" src="images/1.jpg">
+			</div>
+			<div class="col-md-3">
+				<a href="">Produit 2</a>
+				<img class="img-fluid" src="images/2.jpg">
+			</div>
+			<div class="col-md-3">
+				<a href="">Produit 3</a>
+				<img class="img-fluid" src="images/3.jpg">
+			</div>
+			<div class="col-md-3">
+				<a href="">Produit 4</a>
+				<img class="img-fluid" src="images/4.jpg">
+			</div>
+			</div>
+		</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	
+</body>
+</html>
